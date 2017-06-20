d04ae19233e52d20bd9b75aa810130cff529372b
asus\wind <1309233857@qq.com>
2017-06-20 23:11:35 +0800

    侧边栏
    添加侧边栏及样式

--- Echotn/js/home.js
+++ Echotn/js/home.js
@@ -1,4 +1,23 @@
 $(function () {
+    // 侧边栏菜单
+    // 绑定元素点击事件
+    $(".has-children .menu-label").click(function(event) {
+        event.preventDefault();
+        // 判断对象是隐藏还是显示
+        if ($(this).next(".dropdown").is(":hidden")) {
+            // 如果是隐藏
+            if (!$(this).next(".dropdown").is(":animated")) {
+                $(this).addClass("is-active").parent().siblings().find('.menu-label').removeClass('isactive');
+                $(this).next('.dropdown').stop().slideDown().parent().siblings().find('.dropdown').stop().slideUp();
+            }
+        } else {
+            //  显示
+            if (!$(this).next('.dropdown').is(":animated")) {
+                $(this).removeClass("is-active");
+                $(this).next(".dropdown").stop().slideUp();
+            }
+        }
+    });
     // 轮播
     var slider = new Swiper('#slider', {
         pagination: '.swiper-pagination',
