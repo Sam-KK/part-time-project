172c09f6b676b8b2dce3fb4831e20659ea8c4b94
asus\wind <1309233857@qq.com>
2017-07-01 17:54:37 +0800

    jqery
    首页更改jquery路径

--- Echotn/js/about.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    // tab切换
-    $(".nav-tabs li").on("touchend", function () {
-        var $index = $(this).index();
-        $(this).addClass("is-active").siblings().removeClass("is-active");
-        $(".tab-container .tab-pane").eq($index).show().siblings().hide();
-    });
-});
